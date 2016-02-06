811e0ddca646890cb585ea4f5476d61001187386
Farley Knight <Farley Knight>
2016-02-06 13:29:39 -0500

    Add Cards scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131220212) do
+ActiveRecord::Schema.define(version: 20160131224839) do
+
+  create_table "cards", force: :cascade do |t|
+    t.text     "question"
+    t.text     "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
